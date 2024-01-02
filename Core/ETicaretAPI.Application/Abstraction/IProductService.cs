0e2c0c3fbc9863baa02eb33fad0094bea95941f4
ORHUN YUSUF BABAOGLU <orhunyusuf.babaoglu18@ogr.atauni.edu.tr>
2024-01-02 21:00:18 +0300

    eticaret 2.01 update

--- Core/ETicaretAPI.Application/Abstraction/IProductService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ETicaretAPI.Domain.Entities;
-
-namespace ETicaretAPI.Application.Abstraction {
-    public interface IProductService {
-        List<Product> GetProducts();
-
-    }
-}
--- Core/ETicaretAPI.Domain/Entities/Common/BaseEntities.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ETicaretAPI.Domain.Entities.Common {
-    public class BaseEntities {
-        public Guid Id { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
-}
